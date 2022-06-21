564b2700d5ef9fc74b0689fed7be154ba0fac190
KalebLight <kaleb.luiz00@gmail.com>
2022-06-20 23:17:59 -0300

    Criação componente cards-list
    O componente já realiza faz acesso a api através do service, e armazena os dados dos pokemon.

---
+++ src/app/components/cards-list/cards-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/Interface/pokemon';
+import { PokemonListService } from 'src/app/Service/pokemon-list.service';
+
+@Component({
+  selector: 'app-cards-list',
+  templateUrl: './cards-list.component.html',
+  styleUrls: ['./cards-list.component.css'],
+})
+export class CardsListComponent implements OnInit {
+  pokemons: Pokemon[] = [];
+
+  constructor(public pokemonListService: PokemonListService) {}
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    this.pokemonListService.getPokemonsService().subscribe((data) => {
+      this.pokemons = data.results;
+    });
+  }
+}
